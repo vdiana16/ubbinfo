953430da644f6b756e86acd25e30783c3b1d28f2
Pașca Valentina-Diana <dianavalentinapasca@gmail.com>
2025-04-04 12:58:47 +0300

    Add files via upload

---
+++ Second Semester - Operating Systems/Lab3/prob.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	char vocale[] = "aeiou";
+	char sir[] = "aeioubaeiou";
+	int i = 0;
+	char *sir_nou = malloc(stlen(sir)*sizeof(char)+1);
+	return 0;
+}
